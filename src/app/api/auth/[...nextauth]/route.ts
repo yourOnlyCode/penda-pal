8edc6613dc13a31b02192b95625420b36334113e
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2026-01-02 11:05:33 -0500

    fixes to login in prod

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,14 @@ import NextAuth from 'next-auth'
 import { authOptions } from '@/lib/authOptions'
 
 export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
 
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+export async function GET(req: Request) {
+  return await handler(req)
+}
+
+export async function POST(req: Request) {
+  return await handler(req)
+}
